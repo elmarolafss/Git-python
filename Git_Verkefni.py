1a3f1e3942429209c6dc537dc6fafc3c25eeb9e6
elmarolafss <elmarol250@nemi.tskoli.is>
2017-01-24 13:45:49 +0000

    Dæmi 2

--- Git_Verkefni.py
+++ Git_Verkefni.py
@@ -3,5 +3,9 @@ tala1 = int(input("Sláðu inn fyrstu töluna af tvem: "))
 tala2 = int(input("Sláðu inn aðra töluna af tvem: "))
 plus = tala1 + tala2
 sinnum = tala1 * tala2
-print("Tölurnar plúsaðar: %s" % plus)
+print("\nTölurnar plúsaðar: %s" % plus)
 print("Tölurnar margfaldaðar: %s" % sinnum)
+#Dæmi2
+fornafn = input("\nSláðu inn fornafn: ")
+eftirnafn = input("Sláðu in eftirnafn: ")
+print("Halló %s %s" % (fornafn,eftirnafn))
